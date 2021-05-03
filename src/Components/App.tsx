faa807e5beb6eb08590af8a0b45d73f49246a37d
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-05-04 02:35:19 +0300

    Data processing function is written

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,10 +1,63 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from '../services/axios';
+import Context  from '../services/context';
+
+import { IResultData } from '../interfaces/interface';
+import { GlobalStyle } from '../styles/GlobalStyle';
+
+import { Status } from './Status';
+import { Header } from '../Components/Header/Header';
+import { Users } from '../Components/Users/Users';
 
 
-const App = () => {
+const App = (): JSX.Element => {
+    const [usersList, setUsersList] = useState<Array<IResultData>>([]);
+    const [error, setError] = useState<boolean>(false);
+    const [loaded, setLoaded] = useState<boolean>(false);
+
+    useEffect(() => {
+        axios
+        .get('https://randomuser.me/api/?results=15')
+        .then(response => {
+            if (response.data.length !== 0) {
+                setLoaded(true);
+                setUsersList(response.data.results);
+            } else {
+                setError(true);
+            }
+        })
+        .catch((err) => (err.response || err.request) && setError(true))
+    }, []);
+    
+
+    if (error) {
+        return (
+            <>
+            <GlobalStyle />
+                <Header />
+                <Status message="Не удалось подключиться к серверу" />
+            </>
+        )
+    } 
+    
+    if (!loaded) {
+        return (
+            <>
+            <GlobalStyle />
+                <Header />
+                <Status message="Загружаю..." spinner={true} />
+            </>
+        )
+    } 
+
     return (
-        <h1>Test</h1>
+        <Context.Provider value={{ usersList }}>
+        <GlobalStyle />
+            <Header />
+            <Users />
+        </Context.Provider>
     );
+
 }
 
 export default App;
