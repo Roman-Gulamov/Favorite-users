dc3b93baff5b41608669c3f5f703a5ff6f5155e1
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-05-04 02:34:41 +0300

    Added new imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
 import { render } from 'react-dom';
+
 import App from './Components/App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/fonts/lato/stylesheet.css';
 
 
 render(<App />, document.getElementById('root'));
