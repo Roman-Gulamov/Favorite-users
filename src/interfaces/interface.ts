d6e4a4964db8e979355818823e1c5efd05f1b10f
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-05-05 04:34:12 +0300

    Updated types

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -1,10 +1,30 @@
+import { FormEvent, RefObject } from "react";
+
+
 export interface IStatus {
     message: string,
     spinner?: boolean
 }
 
 export interface IContext {
-    usersList: Array<IResultData>
+    usersList: Array<IResultData>,
+    searchRef: RefObject<HTMLDivElement>,
+    searchName: (event: FormEvent<HTMLInputElement>, searchRef: RefObject<HTMLDivElement>) => void
+}
+
+export interface ICardsData extends ICardsProps {
+    eventKey: string,
+    title: string,
+    disabled: boolean
+}
+
+export interface ICardsProps {
+    min: number,
+    max: number
+}
+
+export interface IDisabled {
+    disabled: boolean
 }
 
 export interface IResultData {
--- src/services/context.ts
+++ src/services/context.ts
@@ -1,9 +1,12 @@
+import { FormEvent, RefObject } from 'react';
 import { createContext } from 'react';
 import { IContext } from '../interfaces/interface';
 
 
 const Context = createContext<IContext>({
-    usersList: []
+    usersList: [],
+    searchRef: null,
+    searchName: (event: FormEvent<HTMLInputElement>, searchRef: RefObject<HTMLDivElement>) => {}
 }); 
 
 export default Context;
