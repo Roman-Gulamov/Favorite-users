01dc85456ceadd44d52a7c420dcd5a243867014f
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-05-05 04:33:52 +0300

    Data file for accordion

---
+++ src/data/cardsData.ts
@@ -0,0 +1,33 @@
+import { ICardsData } from "../interfaces/interface";
+
+
+export const CARDS_DATA: Array<ICardsData> = [
+    {
+        eventKey: "1",
+        title: "1-10",
+        min: 1,
+        max: 10,
+        disabled: false
+    },
+    {
+        eventKey: "2",
+        title: "11-20",
+        min: 11,
+        max: 20,
+        disabled: false
+    },
+    {
+        eventKey: "3",
+        title: "21-30",
+        min: 21,
+        max: 30,
+        disabled: true
+    },
+    {
+        eventKey: "4",
+        title: "31-40",
+        min: 31,
+        max: 40,
+        disabled: true
+    }
+]
