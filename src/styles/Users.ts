9bc0f9ed4c8d6a76b075b7d2e5375ab8a0697b6f
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-05-05 04:34:50 +0300

    Styles updated

--- src/styles/Users.ts
+++ src/styles/Users.ts
@@ -1,67 +1,121 @@
-import styled from 'styled-components';
-import { Table } from 'react-bootstrap';
+import styled, { css } from 'styled-components';
+import Card from 'react-bootstrap/Card';
+import { IDisabled } from '../interfaces/interface';
 
 
-export const MyTable = styled(Table)`
-    table-layout: fixed;
-    margin: 1em 0;
+export const UsersWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    border: 4px solid rgba(128, 128, 128, .3);
+    border-radius: 10px;
+`
 
-    @media screen and (max-width: 1024px) {
-        table-layout: auto;
-    }
+export const UsersHeadings = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 8vh;
+    padding: 0 .6em;
+`
 
-    & tr th, & tr td {
+export const HeadingSearch = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 50%;
+
+    & input {
+        border: none;
+        background-color: transparent;
+        transition: background-color 0s 50000s;
+
         @media screen and (max-width: 1024px) {
-            font-size: 0.7em;
-        }
-    }
+            width: 100%;
+            font-size: 0.8em;
+        }   
 
-    & tbody {
-        display: block;
-        max-height: 100vh;
-        overflow-x: hidden;
-        overflow-y: auto;
-
-        &::-webkit-scrollbar { 
-            width: 10px;
-        }
-    
-        &::-webkit-scrollbar-track-piece { 
-            background-color: gray;
-        }
-    
-        &::-webkit-scrollbar-thumb { 
-            background-color: #006B53;
-            border-radius: 15px;
-            height: 100px;
+        &::placeholder {
+            font-weight: 300;
         }
     }
 `
 
-export const UsersSearch = styled.input`
-    width: 100%;
-    border: none;
-    background-color: transparent;
-    transition: background-color 0s 50000s;
-
-    &::placeholder {
-        font-weight: 300;
-    }
+export const HeadingFavorite = styled.span`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    width: 40%;
 
     @media screen and (max-width: 1024px) {
-        width: 100%;
         font-size: 0.8em;
     }
 `
 
-export const Card = styled.div`
+export const UsersList = styled.div`
+    display: flex;
+    flex-direction: row;
+    border-top: 4px solid rgba(128, 128, 128, .3);
+    border-radius: 10px;
+`
+
+export const ListFavorites = styled.div`
+
+`
+
+export const ListWrapper = styled.div`
+    width: 60%;
+`
+
+export const MyCard = styled(Card)<IDisabled>`
+    cursor: pointer;
+
+    & .card-header {
+        @media screen and (max-width: 1024px) {
+            font-size: .7em;
+            text-align: center;
+        }  
+    }
+
+    ${props => props.disabled && css`
+        pointer-events: none;
+        opacity: 60%;
+    `}
+`
+
+export const MyCardBody = styled(Card.Body)`
+    padding: 0;
+    max-height: 100vh;
+    overflow-x: hidden;
+    overflow-y: auto;
+
+    &::-webkit-scrollbar { 
+        width: 0;
+    }
+`
+
+export const CardWrapper = styled.div`
     display: flex;
     padding: .4em;
-    margin-bottom: .5em;
+    border-bottom: 1px solid rgba(128, 128, 128, .3);
+    transition: all .4s ease;
+    cursor: grab;
+    user-select: none;
 
     @media screen and (max-width: 1200px) {
         flex-wrap: wrap;
         justify-content: center;
+    }
+
+    &:hover {
+        background-color: rgba(128, 128, 128, .3);
+    }
+
+    &:active {
+        cursor: grabbing;
+    }
+
+    &:last-child {
+        border-bottom: none;
     }
 `
 
@@ -72,13 +126,11 @@ export const CardImg = styled.div`
     margin-right: 1em;
 
     @media screen and (max-width: 1024px) {
-        width: 100%;
         margin: 0;
     }
     
     & img {
         border-radius: 50%;
-        height: 80%;
     }
 `
 
@@ -140,7 +192,11 @@ export const InfoBlock = styled.div`
 export const BlockMail = styled.p`
     font-size: .9em;
 
-    @media screen and (max-width: 500px) {
+    @media screen and (max-width: 1024px) {
         font-size: .7em;
+    }
+
+    @media screen and (max-width: 500px) {
+        font-size: .5em;
     }
 `
