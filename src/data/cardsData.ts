5a849e920a43f86920c66786990cae6a0597c5c2
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-05-06 05:24:43 +0300

    Removed unnecessary data parameter

--- src/data/cardsData.ts
+++ src/data/cardsData.ts
@@ -6,28 +6,24 @@ export const CARDS_DATA: Array<ICardsData> = [
         eventKey: "1",
         title: "1-10",
         min: 1,
-        max: 10,
-        disabled: false
+        max: 10
     },
     {
         eventKey: "2",
         title: "11-20",
         min: 11,
-        max: 20,
-        disabled: false
+        max: 20
     },
     {
         eventKey: "3",
         title: "21-30",
         min: 21,
-        max: 30,
-        disabled: true
+        max: 30
     },
     {
         eventKey: "4",
         title: "31-40",
         min: 31,
-        max: 40,
-        disabled: true
+        max: 40
     }
 ]
