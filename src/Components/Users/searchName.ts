4da6480e7a0c2a570fb85fb99df582af65413788
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-05-06 05:25:53 +0300

    Search by name function rewritten

--- src/Components/Users/searchName.ts
+++ src/Components/Users/searchName.ts
@@ -1,22 +1,38 @@
 import { FormEvent, RefObject } from 'react';
 
+import findDisabled from './findDisabled';
+
 
 const searchName = (event: FormEvent<HTMLInputElement>, searchRef: RefObject<HTMLDivElement>): void => {
+    const inputValue: string = event.currentTarget.value.toLowerCase().trim();
     const node: HTMLDivElement = searchRef.current;
-    const inputValue: string = event.currentTarget.value;
-    const iterableUsers: NodeListOf<HTMLDivElement> = node.querySelectorAll('[data-text]');
-    
-    if (inputValue.trim()) {
-        iterableUsers.forEach((child: any): string => {
-            return (
-                child.innerText.toLowerCase().trim().indexOf(inputValue.toLowerCase().trim()) === -1 ? 
-                    child.closest("[data-wrapper]").style.display = "none" : 
-                    child.closest("[data-wrapper]").style.display = "flex"
-            );
+    const iterableNames: NodeListOf<HTMLDivElement> = node.querySelectorAll('[data-text]');
+
+    if (inputValue) {
+        iterableNames.forEach((child: HTMLDivElement): void => {
+            let search: number = child.innerText.search(RegExp(inputValue,"gi"));
+
+            if (search === -1) {
+                child.closest("[data-wrapper]").classList.add('hide');
+            } else {
+                child.closest("[data-wrapper]").classList.remove('hide');
+                child.innerHTML = insertMark(child.innerText, search, inputValue.length);
+            }
         });
     } else {
-        iterableUsers.forEach((child: any): string => child.closest("[data-wrapper]").style.display = "flex");
+        iterableNames.forEach((child: HTMLDivElement) => {
+            child.closest("[data-wrapper]").classList.remove('hide');
+            child.innerHTML = child.innerText;
+        });
     }
+
+    findDisabled(searchRef);
+}
+
+const insertMark = (value: string, position: number, amountChar: number): string => {
+    return value.slice(0, position) + 
+            '<mark>' + value.slice(position, position + amountChar) + '</mark>' 
+            + value.slice(position + amountChar)
 }
 
 export default searchName;
