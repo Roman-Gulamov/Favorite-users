1d31a28e92f54845998461503f3e4443585881c7
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-05-06 05:25:04 +0300

    Styles moved to separate files

---
+++ src/styles/CardsItems.ts
@@ -0,0 +1,114 @@
+import styled from 'styled-components';
+
+
+export const CardWrapper = styled.div`
+    display: flex;
+    padding: .4em;
+    border-bottom: 1px solid rgba(128, 128, 128, .3);
+    transition: all .4s ease;
+    cursor: grab;
+    user-select: none;
+
+    @media screen and (max-width: 1200px) {
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    &:hover {
+        background-color: rgba(128, 128, 128, .3);
+    }
+
+    &:active {
+        cursor: grabbing;
+    }
+
+    &:last-child {
+        border-bottom: none;
+    }   
+
+    &.hide {
+        display: none;
+    }
+`
+
+export const CardImg = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-right: 1em;
+
+    @media screen and (max-width: 1024px) {
+        margin: 0;
+    }
+    
+    & img {
+        border-radius: 50%;
+    }
+`
+
+export const CardInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+` 
+
+export const InfoInline = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+
+    @media screen and (max-width: 1024px) {
+        justify-content: center;
+    }
+`
+
+export const InlineName = styled.p`
+    font-style: italic;
+    margin-right: .5em;
+
+    @media screen and (max-width: 1024px) {
+        margin-right: 0;
+    }    
+    
+    @media screen and (max-width: 500px) {
+        font-size: .7em;
+    }
+`
+
+export const InlineRegistered = styled.p`
+    text-align: center;
+
+    @media screen and (max-width: 500px) {
+        font-size: .7em;
+    }
+
+    & span:first-child {
+        font-size: 1em;
+        font-weight: 900;
+
+        @media screen and (max-width: 500px) {
+            font-size: .7em;
+        }
+    }
+
+    & span:last-child {
+        font-size: .9em;
+    }
+`
+
+export const InfoBlock = styled.div`
+    @media screen and (max-width: 1024px) {
+        display: flex;
+        justify-content: center;
+    }
+`
+
+export const BlockMail = styled.p`
+    font-size: .9em;
+
+    @media screen and (max-width: 1024px) {
+        font-size: .7em;
+    }
+
+    @media screen and (max-width: 500px) {
+        font-size: .5em;
+    }
+`
