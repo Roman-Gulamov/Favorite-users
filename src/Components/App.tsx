8df95576ccb619bd577f1f739fe603a2a4cdbe39
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-05-07 05:26:54 +0300

    Completed work on the component

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -16,19 +16,23 @@ const App = (): JSX.Element => {
     const searchRef = useRef<HTMLDivElement | null>(null);
 
     const [usersList, setUsersList] = useState<Array<IResultData>>([]);
+    const [favoriteList, setFavoriteList] = useState<Array<IResultData>>([]);
     const [error, setError] = useState<boolean>(false);
     const [loaded, setLoaded] = useState<boolean>(false);
     const [heartVisibility, setHeartVisibility] = useState<boolean>(false);
     const [dragToFavorite, setDragToFavorite] = useState<boolean>(false);
+    const [currentFavorite, setCurrentFavorite] = useState<IResultData | null>(null);
+    const [currentUser, setCurrentUser] = useState<IResultData | null>(null);
+    const [currentList, setCurrentList] = useState<Array<IResultData> | null>(null);
 
 
     useEffect(() => {
         axios
-        .get('https://randomuser.me/api/?results=50')
+        .get('https://randomuser.me/api/?results=500')
         .then(response => {
             if (response.data.length !== 0) {
                 setLoaded(true);
-                setUsersList(sortData(response.data.results));
+                setUsersList(sorterData(response.data.results));
                 findDisabled(searchRef);
             } else {
                 setError(true);
@@ -38,32 +42,98 @@ const App = (): JSX.Element => {
     }, []); 
 
 
-    const sortData = (data: IResultData[]): Array<IResultData> => {
+    const favoriteWrapperDrop = (event: DragEvent<HTMLDivElement>): void => {
+        event.preventDefault();
+
+        setDragToFavorite(false);
+        setHeartVisibility(false);
+
+        if (currentList) {
+            setFavoriteList(prev => prev.concat(currentUser));
+        
+            setUsersList(prev => 
+                prev.filter(item => item.registered.date !== currentUser.registered.date));
+        }
+    }
+
+    const favoriteDragStart = (card: IResultData): void => {
+        setCurrentFavorite(card);
+        setCurrentList(null);
+    }
+
+    const favoriteDragLeave = (event: DragEvent<HTMLDivElement>): void => {
+        event.currentTarget.classList.remove('gray');
+    }
+
+    const favoriteDragOver = (event: DragEvent<HTMLDivElement>): void => {
+        event.preventDefault();
+        event.currentTarget.classList.add('gray');
+    }
+
+    const favoriteDrop = (event: DragEvent<HTMLDivElement>, card: IResultData): void => {
+        event.preventDefault();
+        event.currentTarget.classList.remove('gray');
+
+        setDragToFavorite(false);
+        setHeartVisibility(false);
+        
+        if (!currentList) {
+            setFavoriteList(prev => 
+                prev.map(item => {
+                    return item.registered.date === card.registered.date ?
+                        {...currentFavorite}
+                    :
+                    item.registered.date === currentFavorite.registered.date ?
+                        {...card}
+                    :
+                        item;
+            }))
+        }
+    }
+
+    const removeFavorite = (card: IResultData): void => {
+        setFavoriteList(prev => 
+            prev.filter(item => item.registered.date !== card.registered.date));
+
+        setUsersList(prev => prev.concat(card));
+    }
+
+    const sorterData = (data: IResultData[]): Array<IResultData> => {
         const sortData = data.sort((prev, next) => prev.registered.age - next.registered.age);
         return sortData;
     }
 
 
-    const dragStartHandler = (event: DragEvent<HTMLDivElement>, card: IResultData) => {
-        setDragToFavorite(true);
+    const usersDragStart = (card: IResultData, list: Array<IResultData>): void => {
         setHeartVisibility(true);
+        setDragToFavorite(true);
+
+        setCurrentUser(card);
+        setCurrentList(list);
     }
 
-    const dragLeaveHandler = (event: DragEvent<HTMLDivElement>) => {
-        
-    }
+    const usersDragEnd = (event: DragEvent<HTMLDivElement>): void => {
+        event.preventDefault();
 
-    const dragEndHandler = (event: DragEvent<HTMLDivElement>) => {
         setDragToFavorite(false);
         setHeartVisibility(false);
     }
 
-    const dragOverHandler = (event: DragEvent<HTMLDivElement>) => {
-        event.preventDefault();
-    }
-
-    const dropHandler = (event: DragEvent<HTMLDivElement>, card: IResultData) => {
-        event.preventDefault();
+    const value = {
+        usersList,
+        favoriteList,
+        searchRef, 
+        searchName,
+        heartVisibility,
+        dragToFavorite,
+        favoriteWrapperDrop,
+        favoriteDragStart, 
+        favoriteDragLeave, 
+        favoriteDragOver, 
+        favoriteDrop,
+        removeFavorite,
+        usersDragStart,
+        usersDragEnd
     }
 
 
@@ -88,18 +158,7 @@ const App = (): JSX.Element => {
     } 
 
     return (
-        <Context.Provider value={{ 
-            usersList, 
-            searchRef, 
-            searchName,
-            heartVisibility,
-            dragToFavorite,
-            dragStartHandler, 
-            dragLeaveHandler, 
-            dragEndHandler, 
-            dragOverHandler, 
-            dropHandler
-        }}>
+        <Context.Provider value={value}>
         <GlobalStyle />
             <Header />
             <Users />
