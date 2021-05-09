1aae5ccbf1e4b583260732dca1713a9716226cb0
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-05-09 14:17:29 +0300

    Added type

--- src/Components/Users/ListFavorites.tsx
+++ src/Components/Users/ListFavorites.tsx
@@ -12,7 +12,7 @@ import {
     CardDelete } from '../../styles/ListFavorites';
 
 
-export const ListFavorites = () => {
+export const ListFavorites = (): JSX.Element => {
     const listProps = useContext(Context);
 
     return (
